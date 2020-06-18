ddb5cfecd961b4be4ecb317282a895b60c86ee84
Isobel <isobelascott@gmail.com>
2020-06-18 02:01:54 -0400

    bin bash thing

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
+#!/usr/bin/python
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-
 @app.route("/30ans")
 def trente():
     return render_template("trente.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
